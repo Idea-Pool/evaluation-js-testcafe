0a7fac9e8d23e86327466eaaf446f1363b467c5c
Adrian Benko <benko.adrian@gmail.com>
2020-04-22 13:48:26 +0200

    Initial commit of the Page objects

---
+++ helpers/pages/angularHomePage.js
@@ -0,0 +1,9 @@
+import { Selector } from 'testcafe';
+export default class AngularHomePage {
+    constructor () {
+        this.angularLogoNavbar = Selector('.nav-link.home img');
+        this.angularLogoHeroSection = Selector('.hero-logo img');
+        this.textInHeroSection = Selector('div.hero-headline');
+        this.getStartedButton = Selector('a.button.hero-cta');
+    }
+}
